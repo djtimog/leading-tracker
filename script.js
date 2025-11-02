ac0f7d4f44eb53cf59cec49b8b08457cb3c7fee2
Christian Ogunleye <117105380+djtimog@users.noreply.github.com>
2025-11-02 08:47:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -21,19 +21,36 @@ tabBtn.addEventListener("click", function () {
 });
 
 function render(leads) {
-  let listItems = "";
+  ulEl.innerHTML = "";
 
-  for (let i = 0; i < leads.length; i++) {
-    listItems += `
-    <li>
-        <a target="_blank" href='${leads[i]}'>
-            ${leads[i]} 
-        </a>
-     </li>
-     `;
-  }
+  leads.reverse().forEach((lead, i) => {
+    const li = document.createElement("li");
 
-  ulEl.innerHTML = listItems;
+    const linkContainer = document.createElement("div");
+    linkContainer.className = "link-container";
+
+    const a = document.createElement("a");
+    a.target = "_blank";
+    a.href = lead;
+    a.textContent = lead;
+
+    const copyBtn = document.createElement("button");
+    copyBtn.className = "copy";
+    copyBtn.textContent = "Copy";
+    copyBtn.addEventListener("click", () => copyLead(lead));
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.className = "cross";
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("dblclick", () => deleteLead(i));
+
+    linkContainer.appendChild(a);
+    li.appendChild(linkContainer);
+    li.appendChild(copyBtn);
+    li.appendChild(deleteBtn);
+
+    ulEl.appendChild(li);
+  });
 }
 
 inputBtn.addEventListener("click", function () {
@@ -49,3 +66,20 @@ deleteBtn.addEventListener("dblclick", function () {
   myLeads = [];
   render(myLeads);
 });
+
+function deleteLead(index) {
+  myLeads.splice(index, 1);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+}
+
+function copyLead(lead) {
+  navigator.clipboard
+    .writeText(lead)
+    .then(() => {
+      alert("Copied the text: " + lead);
+    })
+    .catch((err) => {
+      console.error("Failed to copy: ", err);
+    });
+}
